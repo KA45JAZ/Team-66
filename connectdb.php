f7de7a5bf122cceffeee98ec61bd0bf08cb50d55
Jaspinder Shergill <jaspinder-shergill@hotmail.com>
2026-03-05 13:16:42 +0000

    database connection

--- connectdb.php
+++ connectdb.php
@@ -1,19 +1,12 @@
 <?php
 $host = "localhost";
 $db_name = "cs2team66_db";
-$username = "root";
-$password = ""; // empty for local MySQL
+$username = "cs2team66";
+$password = "2gxAd94ei0twjYviSbNux636R";
 
 try {
-    $db = new PDO(
-        "mysql:host=$host;dbname=$db_name;charset=utf8",
-        $username,
-        $password
-    );
-
+    $db = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
 } catch (PDOException $e) {
     die("Database Connection Failed: " . $e->getMessage());
 }
